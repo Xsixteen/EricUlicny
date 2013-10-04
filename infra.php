94f3b7407cbd2569172798e045656744b48f88ea
Xsixteen <x16.tux@gmail.com>
2013-10-04 00:36:56 +0000

    Initial file push

---
+++ infra.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>-= ericulicny.com =-</title>
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+</head>
+
+<body>
+<div id="wrapper">
+  <!-- <div id=main> -->
+  <div id="top"><br />
+    <div id="logo"></div>
+    <div id="menu-bar">
+    	<a href="index.html">Home</a>
+        <a href="about.php">About</a>
+    </div>
+  </div>
+  <div id="bar"></div>
+  <div id="lg_disp">	<br />
+    <br />
+  	<center>
+  	  <p>&nbsp;</p>
+  	</center>
+  </div>
+  <div id="bar"></div>
+  <div id="content_pane"><br />
+      <center><h1>Projects</h1></center><br>
+      <div id=summary>
+      	Infrastructure projects relate to utilizing the Linux Operating System, scalability, virtualization, and code pushing processes.<br><br>
+	Current projects in this area:  roll tool.<br><br><br><hr>
+
+	<br><br><br>
+	<h2>Roll Tool</h2>
+      </div>
+
+  
+  <div id="footer">(c) 2013 EricUlicny.com</div>
+
+</div>
+</body>
+</html>
---
+++ project.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>-= ericulicny.com =-</title>
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+    <style>
+        .title {
+            font-size: 16px;
+        }
+
+        .desc {
+            font-size: 12px;
+            padding-left: 10px;
+        }
+    </style>
+</head>
+
+<body>
+<div id="wrapper">
+  <!-- <div id=main> -->
+  <div id="top"><br />
+    <div id="logo"></div>
+    <div id="menu-bar">
+    	<a href="index.html">Home</a>
+        <a href="#">About</a>
+        <a href="project.php">Projects</a>	
+    </div>
+  </div>
+  <div id="bar"></div>
+  <div id="lg_disp">	<br />
+    <br />
+  	<center>
+  	  <p>&nbsp;</p>
+  	</center>
+  </div>
+  <div id="bar"></div>
+  <div id="content_pane"><br />
+      <center><h1>Software Projects</h1></center><br>
+      <div id=summary>Below are several software projects I have developed.<br>
+      <ul>
+          <li>
+            <div class="title">Megacrypt</div>
+            <div class = "desc">Megacrypt allows the user to encrypt passwords using a single common password thus making multiple passwords easily manageable.</div>
+          </li>
+          <li>Process Simulator</li>
+          <li>Enigma</li>
+          <li>Genesis</li>
+      </ul>
+  </div>
+
+
+  
+  <div id="footer">(c) 2013 EricUlicny.com</div>
+
+</div>
+</body>
+</html>
