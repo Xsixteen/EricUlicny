c4ce0355c895f2362f5fe4c2db5fe469ef7e2f2f
X16 <x16.tux@gmail.com>
2013-11-19 22:28:12 -0500

    updates to include software stub

---
+++ software.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>-= ericulicny.com =-</title>
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+</head>
+
+<body>
+<div id="wrapper">
+  <!-- <div id=main> -->
+  <div id="top"><br />
+    <div id="logo"></div>
+    <div id="menu-bar">
+    	<a href="index.html">Home</a>
+        <a href="about.php">About</a>
+    </div>
+  </div>
+  <div id="bar"></div>
+  <div id="lg_disp">	<br />
+    <br />
+  	<center>
+  	  <p>&nbsp;</p>
+  	</center>
+  </div>
+  <div id="bar"></div>
+  <div id="content_pane"><br />
+      <center><h1>Software Projects</h1></center><br>
+      <div id=summary>
+      	Software projects relate to programming in Java, Javascript, PHP.<br><br>
+	Current projects in this area:  Sun Tray.<br><br><br><hr>
+
+	<br><br><br>
+	<h2>Sun Tray</h2><br>
+	Sun Tray is a program written in Java as an application which displays information about the Sunset in the OSX tray.  Information such as time until sunset, time until sunrise, and tomorrows length of day.
+      </div>
+
+  
+  <div id="footer">(c) 2013 EricUlicny.com</div>
+
+</div>
+</body>
+</html>
