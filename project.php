675829337c01b4d5a6be4fbb5898a16ac9c6f2f2
X16 <x16.tux@gmail.com>
2013-11-21 20:40:04 -0500

    -Updated <title>

--- project.php
+++ project.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <title>-= ericulicny.com =-</title>
+    <title>EricUlicny.com</title>
     <link rel="stylesheet" type="text/css" href="./css/style.css">
     <style>
         .title {
