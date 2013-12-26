3d463e980fccc809001661a089aefb8517975a42
X16 <x16.tux@gmail.com>
2013-12-26 18:02:06 -0500

    new Page.  Stub for about me created. added hardware button

--- hardware.php
+++ hardware.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <title>EricUlicny.com</title>
+    <title>EricUlicny.com =-</title>
     <link rel="stylesheet" type="text/css" href="./css/style.css">
 </head>
 
@@ -25,9 +25,13 @@
   </div>
   <div id="bar"></div>
   <div id="content_pane"><br />
-      <center><h1>About Me</h1></center><br>
+      <center><h1>Projects</h1></center><br>
       <div id=summary>
-	Still being written.  Please check back shortly.
+      	Infrastructure projects relate to utilizing the Linux Operating System, scalability, virtualization, and code pushing processes.<br><br>
+	Current projects in this area:  roll tool.<br><br><br><hr>
+
+	<br><br><br>
+	<h2>Roll Tool</h2>
       </div>
 
   
