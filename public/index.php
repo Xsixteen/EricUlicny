ff22d7c1ee5db18abc00cec500d81218bd137731
Eric <x16.tux@gmail.com>
2016-06-05 12:42:36 -0400

    basic template changes

--- public/index.php
+++ public/index.php
@@ -5,14 +5,30 @@
         require '../vendor/autoload.php';
 
         $app = new \Slim\App;
-
+        //Setup Extensions
+        $container = $app->getContainer();
+        $container['view'] = function($c) {
+                $view = new \Slim\Views\Twig('../templates', ['debug' => true]);
+                $view->addExtension(
+                new \Slim\Views\TwigExtension(
+                        $c['router']
+                ,$c['request']->getUri()
+                )
+                );
+                $view->addExtension( new \Slim\Views\TwigExtension($c->get('router'), $c->get('request')->getUri()) );
+                $view->addExtension( new \Twig_Extension_Debug() );
+                return $view;
+        };
+        
         //Routes
         require '../src/routes/main.php';
+        
+        //testing
         $app->get('/hello/{name}', function (Request $request, Response $response, $args) {
                 return $response->write("Hello " . $args['name']);
         });
 
        
-
+        //run!
         $app->run();
 ?>
--- src/routes/main.php
+++ src/routes/main.php
@@ -3,7 +3,10 @@
         use \Psr\Http\Message\ResponseInterface as Response;
         
         $app->get('/', function (Request $request, Response $response, $args) {
-                return $response->write("Main Page!  Hello World");
+                return $this->view->render($response, 'home.twig',
+                        [
+                                'title' => 'Eric Ulicny'
+                        ]);
         });
         
 ?>
