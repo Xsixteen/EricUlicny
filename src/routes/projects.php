49b199c2e44f0d1e2ca75a908a2f82c408a87907
Eric <x16.tux@gmail.com>
2016-06-13 19:16:51 -0400

    added additional include

--- src/routes/projects.php
+++ src/routes/projects.php
@@ -1,6 +1,8 @@
 <?php
 use \Service\ProjectService;
 
+use \Mapper\ProjectMapper;
+
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
         
