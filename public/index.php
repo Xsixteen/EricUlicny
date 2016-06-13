cae16765d98c6090f0de9d7e88bae24d8fadc0e4
Eric <x16.tux@gmail.com>
2016-06-13 18:48:10 -0400

    modfied auto load path

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
         use \Psr\Http\Message\ServerRequestInterface as Request;
         use \Psr\Http\Message\ResponseInterface as Response;
 
-        require '../vendor/autoload.php';
+        require '../src/vendor/autoload.php';
         require '../src/config.php';
         
         $app = new \Slim\App;
